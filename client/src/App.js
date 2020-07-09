5e7122ac415c2784eab2b7838f41426985842196
stopachka <stepan.p@gmail.com>
2020-07-09 13:08:45 -0700

    sm

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-function App() {
+function Account() { 
+  return <h1>Welcome to the account page!</h1>
+}
+
+function Home() {
   return (
-    <div className="App">
+    <div className="Home">
       <h1>journaltogether</h1>
       <h3>Keep track of your days and connect with your friends</h3>
       <ol>
@@ -19,12 +24,35 @@ function App() {
       </ol>
       <h4>
         Interested?{" "}
-        <a href="mailto:stepan.p@gmail.com" rel="noopener noreferrer" target="_blank">
+        <a
+          href="mailto:stepan.p@gmail.com"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
           send a ping : )
         </a>
       </h4>
     </div>
   );
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <Switch>
+            <Route path="/u/:id">
+              <Account />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
