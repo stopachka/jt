e173b70ea6bc7c4a8ccc39f9b754dd62657176f8
stopachka <stepan.p@gmail.com>
2020-07-09 20:52:08 -0700

    ugly home

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "./App.css";
-import { withRouter } from "react-router"
+import { withRouter } from "react-router";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import * as firebase from 'firebase/app';
-import qs from 'qs';
+import * as firebase from "firebase/app";
+import qs from "qs";
 
 // Set up Firebase
-import 'firebase/auth';
-import 'firebase/database';
+import "firebase/auth";
+import "firebase/database";
 firebase.initializeApp({
   apiKey: "AIzaSyCBl-YAARDZuf0KcTIOTcmUZjynaKC7puc",
   authDomain: "journaltogether.firebaseapp.com",
@@ -16,48 +16,88 @@ firebase.initializeApp({
   storageBucket: "journaltogether.appspot.com",
   messagingSenderId: "625725315087",
   appId: "1:625725315087:web:a946ce7cf0725381df2b7a",
-  measurementId: "G-GYBL76DZQW"
+  measurementId: "G-GYBL76DZQW",
 });
 
-function serverPath(path) { 
-  const root = process.env.NODE_ENV === 'development'
-    ? 'http://localhost:8080'
-    : ''
-  return `${root}/${path}`
+function serverPath(path) {
+  const root =
+    process.env.NODE_ENV === "development" ? "http://localhost:8080" : "";
+  return `${root}/${path}`;
 }
 
-class AuthComp extends React.Component {
+class MeComp extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      isAuthenticating: true,
+      isLoading: true,
+      isLoggedIn: null,
+      errorMessage: null,
+    };
   }
 
   componentDidMount() {
-    const code = qs.parse(this.props.location.search, { ignoreQueryPrefix: true })['mc'];
-    fetch(
-      serverPath('api/auth'),
-      {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({'magic-code': code}),
-      }
-    ).then(x => x.status === 200 ? x.json() : Promise.reject("uh oh"))
-    .then(({token}) => {
-      firebase.auth().signInWithCustomToken(token);
-    })
-    .catch(() => {
-      console.log('todo');
-    });
-    // Ask server to auth user
-    // get token and validate firebase
-    // listen to changes for firebase
+    window.signOut = () => firebase.auth().signOut();
+    this.handleMagicCode();
+    this.listenToAuth();
   }
 
+  handleMagicCode() {
+    const code = qs.parse(this.props.location.search, {
+      ignoreQueryPrefix: true,
+    })["mc"];
+    if (!code) {
+      this.setState({ isAuthenticating: false });
+      return;
+    }
+    fetch(serverPath("api/auth"), {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ "magic-code": code }),
+    })
+      .then((x) => (x.status === 200 ? x.json() : Promise.reject("uh oh")))
+      .then(({ token }) => firebase.auth().signInWithCustomToken(token))
+      .then(
+        () => {
+          this.setState({ isAuthenticating: false });
+        },
+        () => {
+          this.setState({
+            isAuthenticating: false,
+            errorMessage: "Uh oh. failed to log in please ping Stopa",
+          });
+        }
+      );
+  }
+  listenToAuth() {
+    firebase.auth().onAuthStateChanged((user) => {
+      this.setState({
+        isLoading: false,
+        isLoggedIn: !!user,
+      });
+    });
+  }
   render() {
-    return <h1>Welcome to the account page!</h1>
+    const {
+      isLoading,
+      isAuthenticating,
+      isLoggedIn,
+      errorMessage,
+    } = this.state;
+    if (errorMessage) {
+      return <div>Uh oh we got an error</div>;
+    }
+    if (isLoading || isAuthenticating) {
+      return <div>Loading...</div>;
+    }
+    if (!isLoggedIn) {
+      return <div>Log in!</div>;
+    }
+    return <h1>You are looged in!</h1>;
   }
 }
 
-const Auth = withRouter(AuthComp);
+const Me = withRouter(MeComp);
 
 function Home() {
   return (
@@ -95,8 +135,8 @@ class App extends React.Component {
       <div className="App">
         <Router>
           <Switch>
-            <Route path="/mc-auth">
-              <Auth />
+            <Route path="/me">
+              <Me />
             </Route>
             <Route path="/">
               <Home />
